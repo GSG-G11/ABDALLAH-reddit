6aea87b281f858bd873cd085a0644051f5251ea5
AbdallahGot <abdallah897107@gmail.com>
2022-03-14 15:08:57 +0200

    add build.sql file and queries folder with addUser queries relates #4

--- server/app.js
+++ server/app.js
@@ -3,6 +3,6 @@ const path = require('path');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '..', 'public')));
-app.set('port', process.env.port || 3030);
+app.set('port', process.env.port || 5050);
 
 module.exports = app;
